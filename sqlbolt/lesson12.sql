5e8c18b57c924ef17549f08c002d83d1deb8486b
nahikhvn <nahikhan@proton.me>
2026-03-28 18:55:47 -0400

    more select query prac

---
+++ sqlbolt/lesson12.sql
@@ -0,0 +1,9 @@
+SELECT Director, COUNT(Director) AS directed
+FROM Movies
+    GROUP BY Director
+
+SELECT director, SUM(domestic_sales + international_sales) as Cumulative_sales_from_all_movies
+FROM movies
+    INNER JOIN boxoffice
+        ON movies.id = boxoffice.movie_id
+GROUP BY director;
