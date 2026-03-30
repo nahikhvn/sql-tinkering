07aac234d2267461729e934fd50647e8746cee63
nahikhvn <nahikhan@proton.me>
2026-03-30 17:05:38 -0400

    updating tables

---
+++ sqlbolt/lesson14.sql
@@ -0,0 +1,13 @@
+UPDATE Movies
+SET Director = 'John Lasseter'
+WHERE Title = "A Bug's Life";
+
+UPDATE Movies
+SET Year = 1999
+WHERE Title = 'Toy Story 2';
+
+UPDATE Movies
+SET Title = 'Toy Story 3',
+    Director = 'Lee Unkrich'
+WHERE Title = 'Toy Story 8';
+    
